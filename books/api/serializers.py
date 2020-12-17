5b21df9111f0a96d3b03bbdae3792167e674747a
wagner <wagnersr92@gmail.com>
2020-12-17 01:22:00 -0300

    Correcao no nome do serializer

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
-from models import Books
+from books.models import Books
 
-class BookSerializer(serializers.ModelSerializer):
+class BooksSerializer(serializers.ModelSerializer):
     class Meta:
         model = Books
         fields = '__all__'
